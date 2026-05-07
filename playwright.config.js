7f99ca5c4936386f887f64fc651a5764a670d126
Gleb Arestov <gr1b0k@gmail.com>
2026-05-07 09:41:16 +0300

    chore: fullyParallel

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
 	testDir: './tests/integration',
+	fullyParallel: true,
 	timeout: 30_000,
 	use: {
 		baseURL: 'http://127.0.0.1:4174',
